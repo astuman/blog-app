ca4429665c3552851fe051a160cd20f3e29d7164
Astewul <loloalex1992@gmail.com>
2023-02-17 12:22:50 +0300

    update 3

--- blogger2/src/store/store.js
+++ blogger2/src/store/store.js
@@ -5,9 +5,7 @@ import thunk from 'redux-thunk';
 import { FETCH_POSTS } from '../actions/types';
 import { PostReducer } from '../reducer/postReducer';
 
- const Store = createStore (rootReducer);
+ export const Store = createStore (rootReducer);
 
 
 export default Store;
-const store = createStore(rootReducer)
-console.log('fvdd',store.dispatch(PostReducer.FETCH_POSTS))
